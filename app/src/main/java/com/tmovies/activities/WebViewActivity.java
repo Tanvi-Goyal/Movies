90d971a88081e5e98545b0e33fbe4fec6e891e55
tanvi <goyaltanvi94@gmail.com>
2020-04-19 14:57:49 +0530

    Completed Webview Activity

--- app/src/main/java/com/tmovies/activities/WebViewActivity.java
+++ app/src/main/java/com/tmovies/activities/WebViewActivity.java
@@ -1,17 +1,20 @@
 package com.tmovies.activities;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.tmovies.R;
-import com.tmovies.utils.MyWebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -19,17 +22,19 @@ public class WebViewActivity extends AppCompatActivity {
     String url;
     private WebView webView;
     private FloatingActionButton backward, forward;
-    ProgressDialog pd = null;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        showProcessDialog();
         position = getIntent().getStringExtra("position");
         url = getIntent().getStringExtra("detail_url");
-        this.webView = findViewById(R.id.web_view);
+
+        pd = new ProgressDialog(this);
+
+        webView = findViewById(R.id.web_view);
         backward = findViewById(R.id.btn_backward);
         forward = findViewById(R.id.btn_forward);
 
@@ -38,42 +43,13 @@ public class WebViewActivity extends AppCompatActivity {
 
         MyWebViewClient webViewClient = new MyWebViewClient();
         webView.setWebViewClient(webViewClient);
-
         webView.loadUrl(url);
 
-        int DELAY = 5000;
+        setBackBtnListener();
+        setForwardBtnListener();
+    }
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                hideProcessDialog();
-
-            }
-        }, DELAY);
-
-        backward.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int currentPosition = Integer.parseInt(position);
-
-                if (Integer.parseInt(position) <= 0) {
-                    backward.setClickable(false);
-                    Toast.makeText(WebViewActivity.this, getString(R.string.no_previous_movie), Toast.LENGTH_SHORT).show();
-                } else {
-                    if (MovieScreen.adapter.getItemCount() > --currentPosition) {
-                        url = MovieScreen.adapter.getItem(currentPosition).getDetail_url();
-                        webView.loadUrl(url);
-                    } else {
-                        Toast.makeText(WebViewActivity.this, getString(R.string.no_previous_movie), Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-                position = String.valueOf(currentPosition);
-
-            }
-        });
-
+    private void setForwardBtnListener() {
         forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,17 +71,61 @@ public class WebViewActivity extends AppCompatActivity {
         });
     }
 
+    private void setBackBtnListener() {
+        backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentPosition = Integer.parseInt(position);
+
+                if (Integer.parseInt(position) <= 0) {
+                    backward.setClickable(false);
+                    Toast.makeText(WebViewActivity.this, getString(R.string.no_previous_movie), Toast.LENGTH_SHORT).show();
+                } else {
+                    if (MovieScreen.adapter.getItemCount() > --currentPosition) {
+                        url = MovieScreen.adapter.getItem(currentPosition).getDetail_url();
+                        webView.loadUrl(url);
+                    } else {
+                        Toast.makeText(WebViewActivity.this, getString(R.string.no_previous_movie), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                position = String.valueOf(currentPosition);
+            }
+        });
+    }
+
     public void showProcessDialog() {
-        pd = new ProgressDialog(this);
-        pd.setTitle("Please Wait...");
-        pd.setMessage("Loading Info");
-        pd.setCancelable(false);
-        pd.show();
+        if (!pd.isShowing()) {
+            pd.setTitle("Please Wait...");
+            pd.setMessage("Loading Info");
+            pd.setCancelable(false);
+            pd.show();
+        }
     }
 
     public void hideProcessDialog() {
         if (pd.isShowing()) {
             pd.dismiss();
+        }
+    }
+
+    public class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            return false;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            showProcessDialog();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            hideProcessDialog();
         }
     }
 
